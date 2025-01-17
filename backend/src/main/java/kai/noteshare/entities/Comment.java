bb87309a8305ed68ab3c0cbda95e9d4b47822bb1
Dawid Siwak <dawidsiwak2137@gmail.com>
2025-01-17 20:55:42 +0100

    feat: add user associations to Note, Folder, and Comment entities and create migration for user_id in notes table

--- backend/src/main/java/kai/noteshare/entities/Comment.java
+++ backend/src/main/java/kai/noteshare/entities/Comment.java
@@ -9,7 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
+import lombok.Data;
+@Data
 @Entity
 @Table(name = "comments")
 public class Comment {
